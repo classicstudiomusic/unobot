94f1aee2e3e303cf3931c44cc75afc3d525a4c2c
classicstudiomusic <iniebelahsini@gmail.com>
2026-03-13 00:14:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const games = new Map();
 
 client.once('ready', () => {
   console.log(`✅ Bot ${client.user.tag} sudah online!`);
-  client.user.setActivity('🃏 UNO | !uno help');
+  client.user.setActivity('🃏 UNO by Lx (CLASSIC STUDIO LAB)| !uno help');
 });
 
 client.on('messageCreate', async (message) => {
